6ba60e86e2fb1a3c9e6cced571931c06d04c11ad
alesanjinesp <alepeceros2022@gmail.com>
2023-10-13 11:48:47 -0500

    Agregando funciones

--- javascript.js
+++ javascript.js
@@ -1,8 +1,27 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail'); 
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+iconMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
-    
+    aside.classList.add('inactive')
+
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive')
+}
+
+function toggleCarritoAside(){
+    mobileMenu.classList.add('inactive'),
+    desktopMenu.classList.add('inactive'),
+    aside.classList.toggle('inactive')
 }
